9a2206b87238de4e2fa8140b0e5e164e262f8f70
rastridge <rfa@me.com>
2024-11-07 11:01:05 -0500

    change caption

--- app/components/ImageGallery.vue
+++ app/components/ImageGallery.vue
@@ -1,34 +1,35 @@
 <script setup lang="ts">
-const { data: images, refresh } = await useFetch('/api/images')
+const { data: images, refresh } = await useFetch("/api/images");
 
-async function uploadImage (e: Event) {
+async function uploadImage(e: Event) {
   // https://hub.nuxt.com/docs/storage/blob#useupload
-  const upload = useUpload('/api/images/upload', {
-    multiple: false
-  })
-  const form = e.target as HTMLFormElement
+  const upload = useUpload("/api/images/upload", {
+    multiple: false,
+  });
+  const form = e.target as HTMLFormElement;
   await upload(form.image)
     .then(async () => {
-      form.reset()
-      await refresh()
+      form.reset();
+      await refresh();
     })
-    .catch((err) => alert('Failed to upload image:\n'+ err.data?.message))
+    .catch((err) => alert("Failed to upload image:\n" + err.data?.message));
 }
 
-async function deleteImage (pathname: string) {
-  await $fetch(`/api/images/${pathname}`, { method: 'DELETE' })
-  await refresh()
+async function deleteImage(pathname: string) {
+  await $fetch(`/api/images/${pathname}`, { method: "DELETE" });
+  await refresh();
 }
 </script>
 
 <template>
   <div>
-    <h3>Images</h3>
+    <h3>Images by Astridge</h3>
     <form @submit.prevent="uploadImage">
-      <label>Upload an image: <input type="file" name="image" accept="image/jpg,image/png"></label>
-      <button type="submit">
-        Upload
-      </button>
+      <label
+        >Upload an image:
+        <input type="file" name="image" accept="image/jpg,image/png"
+      /></label>
+      <button type="submit">Upload</button>
     </form>
     <p>
       <img
@@ -38,7 +39,7 @@ async function deleteImage (pathname: string) {
         :src="`/images/${image.pathname}`"
         :alt="image.pathname"
         @dblclick="deleteImage(image.pathname)"
-      >
+      />
     </p>
     <p v-if="images?.length">
       <i>Tip: delete an image by double-clicking on it.</i>
